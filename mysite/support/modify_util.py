55f98f51fa1e3eb20badcd0b8ace713cb1cb04b9
maxgarvey <maxgarvey@gmail.com>
2012-10-19 12:27:44 -0700

    changed some more prints

--- mysite/support/modify_util.py
+++ mysite/support/modify_util.py
@@ -1,8 +1,8 @@
 def modify_process_form( form ):
     '''this method gets the different fields from the calendar form'''
-    print "form_submit:\n\n{0}\n".format(form.cleaned_data) #debug
+    #print "form_submit:\n\n{0}\n".format(form.cleaned_data) #debug
     #print 'form_group_name: {0}'.format(form.cleaned_data['modify_group_name']) #debug
-    return form.cleaned_data['group_dn'], form.cleaned_data['modify_group_name'], \
+    return form.data['group_dn'], form.cleaned_data['modify_group_name'], \
         form.cleaned_data['group_preferredcn'], form.cleaned_data['group_room'], \
         form.cleaned_data['group_phone'], form.cleaned_data['group_email'], \
         form.cleaned_data['group_labeledUri']
